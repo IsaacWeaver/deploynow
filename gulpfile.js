d25e191f2232bdd96b7f0ac781577db22a27f89d
Isaac <isaac@weaverinnovations.com>
2019-08-02 14:04:19 -0500

    Update gulpfile.js
    changed defualt directory for source files, added babel to the pipe for compiling java script.

--- gulpfile.js
+++ gulpfile.js
@@ -10,10 +10,12 @@ var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+
 
 // Sass task: compiles the style.scss file into style.css
 gulp.task('sass', function(){
-    return gulp.src('./sass/*.scss')
+    return gulp.src('./src/sass/*.scss')
         .pipe(sass()) // compile SCSS to CSS
         .pipe(cssnano()) // minify CSS
         .pipe(gulp.dest('website')); // put final CSS in dist folder
@@ -21,16 +23,17 @@ gulp.task('sass', function(){
 
 // JS task: concatenates and uglifies JS files to script.js
 gulp.task('js', function(){
-    return gulp.src(['./js/*.js'])
-        .pipe(concat('all.js'))
-        // .pipe(uglify())
+    return gulp.src(['./src/js/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(babel())
+        .pipe(uglify())
         .pipe(gulp.dest('website'));
 });
 
 // Watch task: watch SCSS and JS files for changes
 gulp.task('watch', function(){
-    gulp.watch('./sass/*.scss', gulp.series('sass'));
-    gulp.watch('./js/*.js', gulp.series('js'));    
+    gulp.watch('./src/sass/*.scss', gulp.series('sass'));
+    gulp.watch('./src/js/*.js', gulp.series('js'));    
 });
 
 // Default task
